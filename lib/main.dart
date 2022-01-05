8119f1e6fc048f18057569cde9d2fdc28f5c85c1
dmsrgv <ucozar@gmail.com>
2022-01-05 10:52:22 +0300

    add push notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,54 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobileshop/components/map_screen.dart';
 import 'package:mobileshop/components/nav_bar.dart';
-import 'package:mobileshop/components/nav_bar.dart';
+import 'package:mobileshop/components/navigator_helper.dart';
+import 'package:mobileshop/features/cart/presentation/pages/cart_screen.dart';
 import 'package:mobileshop/features/home/presentation/bloc/home_bloc.dart';
-import 'package:mobileshop/features/home/presentation/pages/home_screen.dart';
 import 'package:mobileshop/location_service.dart' as di;
-
 import 'features/cart/presentation/bloc/cart_bloc.dart';
 import 'features/detail/presentation/bloc/detail_bloc.dart';
+import 'features/detail/presentation/pages/product_details_screen.dart';
 import 'location_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await di.init();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  static final GlobalKey<NavigatorState> navKey = GlobalKey<NavigatorState>();
+  Future<void> setupInteractedMessage() async {
+    RemoteMessage? initialMessage =
+        await FirebaseMessaging.instance.getInitialMessage();
+
+    if (initialMessage != null) {
+      _handleMessage(initialMessage);
+    }
+
+    FirebaseMessaging.onMessageOpenedApp.listen(_handleMessage);
+  }
+
+  void _handleMessage(RemoteMessage message) {
+    navKey.currentState!.pushNamed('/detail');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupInteractedMessage();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,9 +62,15 @@ class MyApp extends StatelessWidget {
           BlocProvider<CartBloc>(
               create: (context) => sl<CartBloc>()..add(LoadCartScreenEvent()))
         ],
-        child: const MaterialApp(
+        child: MaterialApp(
+          navigatorKey: navKey,
           debugShowCheckedModeBanner: false,
-          home: NavBar(),
+          home: const NavBar(),
+          routes: {
+            '/mapscreen': (context) => const MapScreen(),
+            '/cart': (context) => const CartScreen(),
+            '/detail': (context) => const ProductDetailsScreen(),
+          },
         ));
   }
 }
